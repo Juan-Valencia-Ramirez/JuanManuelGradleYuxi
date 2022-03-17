08d64ce99362889e16b649c88f379b7bb4407d2d
emilio.marin <emilio.marin@YUXMED0002L>
2022-03-17 10:34:24 -0500

    adding new files

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,29 @@
+package steps;
+
+import cucumber.api.java.en.*;
+
+
+public class GoogleSteps {
+    
+
+    @Given("^I am on the Google search page$")
+    public void navigateToGoogle(){
+        
+    }
+
+    @When("^I search for a criteria$")
+    public void searchCriteria(){
+        
+    }
+
+    @And("^I click on the search button$")
+    public void clickOnSearchButton(){
+        
+    }
+
+    @Then("^the result is displayed on screen$")
+    public void validateResutls(){
+        
+    }
+
+}
